4139901249f14ed5eb8d56fe09298d0b92809e1c
hescoval <hugovieiraescoval@gmail.com>
2023-10-23 18:39:48 +0100

    More changes

--- webscraping/continente.py
+++ webscraping/continente.py
@@ -1,27 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+def get_continente(ean):
+	url = "https://www.continente.pt/pesquisa/?q=5601012001310"
+	response = requests.get(url)
 
-url = "https://www.continente.pt/pesquisa/?q=5601012001310"
-response = requests.get(url)
+	soup = BeautifulSoup(response.content, "html.parser")
 
-soup = BeautifulSoup(response.content, "html.parser")
+	product = soup.find_all('div', class_='ct-image-container')[0]
 
-product = soup.find_all('div', class_='ct-image-container')[0]
+	product_link = product.find_all('a')[0]['href']
 
-product_link = product.find_all('a')[0]['href']
+	website = "Continente"
 
-website = "Continente"
+	response = requests.get(product_link)
 
-response = requests.get(product_link)
+	soup = BeautifulSoup(response.content, "html.parser")
 
-soup = BeautifulSoup(response.content, "html.parser")
+	price = soup.find_all('span', class_='ct-price-formatted')[0].text
 
-price = soup.find_all('span', class_='ct-price-formatted')[0].text
+	name = soup.find_all('h1', class_='product-name')[0].text
 
-name = soup.find_all('h1', class_='product-name')[0].text
-
-capacity = (soup.find_all('span', class_='ct-pdp--unit col-pdp--unit'))[0].text
-capacity = capacity.replace("garrafa ", "")
+	capacity = (soup.find_all('span', class_='ct-pdp--unit col-pdp--unit'))[0].text
+	capacity = capacity.replace("garrafa ", "")
 
 
-print(name, website, price, capacity)
+	print("From website" + website)
+	print("Bottle name" + name)
+	print("Current price is" + price)
+	print("Bottle capacity is" + capacity)
