03a4425ff440082760027e5ff3ccb39522de384f
brumarti <brumarti@student.42porto.com>
2023-10-23 17:58:42 +0100

    FIrst setup

---
+++ main.py
@@ -0,0 +1,5 @@
+from webscraping.continente import get_continente
+ean = input('EAN: ')
+
+print(ean)
+get_continente(ean)
---
+++ webscraping/continente.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_continente(ean):
+	url = "https://www.continente.pt/pesquisa/?q=" + str(ean)
+	response = requests.get(url)
+
+	soup = BeautifulSoup(response.content, "html.parser")
+
+	product = soup.find_all('div', class_='ct-image-container')[0]
+
+	product_link = product.find_all('a')[0]['href']
+	print(product_link)
+
+	response = requests.get(product_link)
+
+	soup = BeautifulSoup(response.content, "html.parser")
+
+	price = soup.find_all('span', class_='ct-price-formatted')[0].text
+	name = soup.find_all('h1', class_='product-name')[0].text
+
+	print(price, name)
