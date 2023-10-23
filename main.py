93e9d4f880e3c34d19b4076d4eb004bd0b4d789c
ccosta-c <ccosta-c@student.42porto.com>
2023-10-23 18:29:15 +0100

    added garrafeira soares

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from webscraping.continente import get_continente
+from webscraping.garrafeirasoares import get_garrafeira_soares
 ean = input('EAN: ')
 
 print(ean)
 get_continente(ean)
+get_garrafeira_soares(ean)
