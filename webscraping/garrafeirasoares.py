e776657fc5822a285933d2aa5ab4465647128b16
ccosta-c <ccosta-c@student.42porto.com>
2023-10-23 18:24:19 +0100

    garrafeira soares and main

---
+++ webscraping/garrafeirasoares.py
@@ -0,0 +1,34 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+
+def find_and_save_character(string, character):
+  match = re.search(r'{}'.format(character), string)
+  if match:
+    saved_character = match.group()
+    new_string = string.replace(saved_character, '')
+    return new_string, saved_character
+  else:
+    return string, None
+
+def get_garrafeira_soares(ean):
+    url = "https://www.garrafeirasoares.pt/pt/resultado-da-pesquisa_36.html?term=" + str(ean)
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    link = soup.find_all('script')[0]
+    url_match = re.search(r"location='(.*?)'", link.text)
+    url = url_match.group(1)
+
+    response = requests.get(url)
+    
+    soup = BeautifulSoup(response.content, "html.parser")
+    price = soup.find_all('h2', class_='clearfix')[0].text
+    name = soup.find_all('div', class_='name clearfix')[0].text
+    price = price.replace('\t', '')
+    price,currency = find_and_save_character(price, '€')
+    price = price.replace(' ', '')
+
+    print(name, price, currency)
