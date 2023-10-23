2c39188728d5300db9cff6b05a6cdab66c4d6255
hescoval <hugovieiraescoval@gmail.com>
2023-10-23 21:31:23 +0100

    More changes to continente scrapper

--- webscraping/continente.py
+++ webscraping/continente.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 def get_continente(ean):
 	url = "https://www.continente.pt/pesquisa/?q="
+	ean = input('EAN: ')
 	url = url + ean
 	response = requests.get(url)
 
@@ -11,23 +12,32 @@ def get_continente(ean):
 
 	product_link = product.find_all('a')[0]['href']
 
-	website = "Continente"
-
 	response = requests.get(product_link)
 
 	soup = BeautifulSoup(response.content, "html.parser")
 
-	price = soup.find_all('span', class_='ct-price-formatted')[0].text
+	total = soup.find_all('span', class_='ct-price-formatted')[0].text
+	total = total.replace('\n', '')
+	currency = total[0]
+	price = total[1::1]
 
 	name = soup.find_all('h1', class_='product-name')[0].text
+	name = name.replace("\n", "")
 
 	capacity = (soup.find_all('span', class_='ct-pdp--unit col-pdp--unit'))[0].text
 	capacity = capacity.replace("garrafa ", "")
+	capacity = capacity.replace("\n", "")
+	capacity = capacity.replace(" cl", "")
+	capacity = capacity + "0"
 
 	origem = (soup.find_all('p', class_='mb-20'))[2].text
+	origem = origem.replace('\n', '')
 
-	print("From website " + website)
-	print("Bottle name" + name)
-	print("Bottle capacity is" + capacity)
-	print("Current price is" + price)
-	print("Origin / Region" + origem)
+	print("Name: " + name)
+	print("Ano: " + str(None))
+	print("Capacidade (em ML): " + capacity)
+	print("EAN: " + str(ean))
+	print("Link: " + product_link)
+	print("Price: " + price)
+	print("Currency: " + currency)
+	print("Localização: " + origem)
