97d298f420202914566cddb7b2a20a2eb42194a1
brumarti <brumarti@student.42porto.com>
2023-10-23 18:39:40 +0100

    elingles start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from webscraping.continente import get_continente
+from webscraping.elingles import get_elingles
 ean = input('EAN: ')
 
 print(ean)
-get_continente(ean)
+#get_continente(ean)
+get_elingles(ean)
--- webscraping/elingles.py
+++ webscraping/elingles.py
@@ -2,4 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_elingles(ean):
-	url = "https://www.elcorteingles.pt/supermercado/?term=" + str(ean)
+	url = "https://www.elcorteingles.pt/supermercado/pesquisar/?term=" + str(ean)
+
+	response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0'})
+	soup = BeautifulSoup(response.content, 'html.parser')
+
+	print(soup)
+	product_link = soup.find_all('a', class_='product-link')[0]['href']
+	print(product_link)
