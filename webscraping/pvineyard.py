16a501151131664d889e51d2a39116b94728421d
brumarti <brumarti@student.42porto.com>
2023-10-25 14:08:07 +0100

    fixed errors

--- webscraping/pvineyard.py
+++ webscraping/pvineyard.py
@@ -7,10 +7,12 @@ import requests
 def get_pvineyard(ean):
 	url = 'https://www.portugalvineyards.com/pt/search?s=' + str(ean)
 
-	
 	response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0'})
 	soup = BeautifulSoup(response.content, 'html.parser')
 
-	product_link = soup.find_all('a', class_='thumbnail product-thubmnail')[0]['href'].text
+	file1 = open("myfile.txt","w")
+	file1.write(str(soup))
+
+	product_link = soup.find_all('a', class_='thumbnail product-thumbnail')[0]['href']
 	response = requests.get(product_link, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0'})
 	soup = BeautifulSoup(response.content, 'html.parser')
